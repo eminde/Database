a38c39ceb3424d57c008b4e06ab759223e910bc2
eminde <ati.amin.de@gmail.com>
2019-02-15 02:18:35 +0330

    Add main for console representation of program

---
+++ main_console.py
@@ -0,0 +1,40 @@
+
+import os
+
+# a function to clear the terminal
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def  main():
+    print("1 - Add a Customer")
+    print("2 - Delete a Customer")
+    print("1 - Print Banks Records(Sorted By Name)")
+    print("1 - Print Banks Records(Sorted By Value)")
+    print("1 - Print Banks Assets")
+    print("1 - Exit")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
